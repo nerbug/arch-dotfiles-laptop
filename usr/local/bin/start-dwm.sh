95ea8b6e9fedf2ef1bdbf184082c0e6bf9252c7b
Aldas Taraškevičius <aldas60@gmail.com>
2020-06-09 13:29:12 +0300

    Remove unnecessary background wallpaper setup

--- usr/local/bin/start-dwm.sh
+++ usr/local/bin/start-dwm.sh
@@ -3,13 +3,6 @@
 # set terminal
 export TERM=st
 
-# set background
-if [ -f ~/.config/dwm-background.jpg ]; then
-    wal -i ~/.config/dwm-background.jpg
-else
-    xsetroot -solid "#000000"
-fi
-
 setbg &
 xcompmgr &
 dwmblocks &
