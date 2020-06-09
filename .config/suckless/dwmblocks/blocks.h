93ce5c5ed2bc1122c2e210d37daeb3641cd5d587
Aldas Taraškevičius <aldas60@gmail.com>
2020-06-09 11:51:27 +0300

    Initial commit
    Initial commit of various Manjaro dotfiles

---
+++ .config/suckless/dwmblocks/blocks.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
+	{"",		"~/.local/bin/statusbar/volume",		0,			10},
+	{"",		"~/.local/bin/statusbar/battery",		60,			 0},
+	{"",		"~/.local/bin/statusbar/mem_usage",		10,			 0},
+	{"",		"~/.local/bin/statusbar/clock",			1,			 0}
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
---
+++ .zprofile
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+export EDITOR="nvim"
+export BROWSER="firefox"
+
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+export XDG_CONFIG_DIRS="/etc/xdg"
+export LESSHISTFILE="-"
+export ZDOTDIR="$XDG_CONFIG_HOME"/zsh
