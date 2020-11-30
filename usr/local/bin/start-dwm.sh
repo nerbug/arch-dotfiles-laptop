1d8bac5144c3e37603cd2498fb8a23b716a4f94d
Aldas Taraškevičius <aldas60@gmail.com>
2020-11-30 18:36:00 +0200

    Use Breeze theme for Qt, remove unneeded stuff

--- usr/local/bin/start-dwm.sh
+++ usr/local/bin/start-dwm.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# set terminal
-export TERM=st
-# make Qt5 apps have the same color scheme as non-Qt apps
-export QT_STYLE_OVERRIDE=gtk2
+export QT_STYLE_OVERRIDE=Breeze
 
 setbg &
 (sleep 1 && picom --experimental-backends) &
