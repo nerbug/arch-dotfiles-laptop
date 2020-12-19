12489387e445494fb1b55cf2ce671b7e55681548
Aldas Taraškevičius <aldas60@gmail.com>
2020-12-19 12:55:04 +0200

    Add comment in start-dwm.sh

--- usr/local/bin/start-dwm.sh
+++ usr/local/bin/start-dwm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Fake QT applications into thinking they are running under KDE, so that it uses KDE's config files.
+# Probably temporary, until I find a less hacky solution.
 export XDG_CURRENT_DESKTOP=KDE
 
 setbg &
