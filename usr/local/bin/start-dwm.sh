fae71638bc868091b56277940254592ce8e1399b
Aldas Taraškevičius <aldas60@gmail.com>
2020-12-24 13:04:52 +0200

    Reorder dwmblocks to start first
    Noticed that dwmblocks takes a little bit of time to start up. Moved it
    in the start-dwm.sh script to start up sooner.

--- usr/local/bin/start-dwm.sh
+++ usr/local/bin/start-dwm.sh
@@ -3,9 +3,9 @@
 # Use settings from KDE for Qt apps (alternatively, setup qt5ct)
 export XDG_CURRENT_DESKTOP=KDE
 
+dwmblocks &
 setbg &
 (sleep 1 && picom --experimental-backends) &
-dwmblocks &
 /usr/lib/polkit-kde-authentication-agent-1 &
 xrdb ~/.config/x11/Xresources
 exec dwm
